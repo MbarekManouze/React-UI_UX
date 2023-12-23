0ee9283e83dc998da3b5878b30ef5013ef6d209a
MbarekManouze <ecloesbac2020@gmail.com>
2023-12-23 17:43:36 +0100

    React App just getting started

---
+++ bank_modern_app/src/components/index.js
@@ -0,0 +1,29 @@
+import Billing from './Billing'
+import Business from './Business'
+import Button from './Button'
+import CardDeal from './CardDeal'
+import Clients from './Clients'
+import FeedbackCard from './FeedbackCard'
+import Footer from './Footer'
+import Getstarted from './Getstarted'
+import Hero from './Hero'
+import Navbar from './Navbar'
+import Stats from './Stats'
+import Testimonials from './Testimonials'
+import CTA from './CTA'
+
+export {
+    Billing,
+    Business,
+    Button,
+    CardDeal,
+    Clients,
+    FeedbackCard,
+    Footer,
+    Getstarted,
+    Hero,
+    Navbar,
+    Stats,
+    Testimonials,
+    CTA,
+};
