2a09502c49b60a2570e3910a58ecf2c2b5a36492
MbarekManouze <ecloesbac2020@gmail.com>
2023-12-26 15:12:45 +0100

    this is getting exciting

--- bank_modern_app/src/components/Button.jsx
+++ bank_modern_app/src/components/Button.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
 
-const Button = () => {
+const Button = ({styles}) => {
   return (
-    <div>Button</div>
+    <div className={styles}>
+      <button type="button" className={`py-4 px-6 
+      bg-blue-gradient font-poppins font-medium text-[18px] 
+      text-primary outline-none rounded-2xl`}>
+        Get Started
+      </button>
+    </div>
   )
 }
 
--- bank_modern_app/src/components/Navbar.jsx
+++ bank_modern_app/src/components/Navbar.jsx
@@ -19,12 +19,31 @@ const Navbar = () => {
           </li>
         ))}
       </ul>
-      <div className="sm-hidden flex flex-1 justify-end items-center">
+
+      <div className="sm:hidden flex flex-1 justify-end items-center">
           <img src={first ? close : menu} 
             alt="menu"
             className="w-[28px] h-[28px] object-contain"
             onClick={() => setfirst((prev) => !prev)}
             />
+
+            <div className={`${first ? 'flex' : 'hidden'} p-6 bg-black-gradient 
+                              absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}>
+              
+              <ul className="list-none flex justify-end items-center flex-1 flex-col">
+                
+                {navLinks.map((nav, index) => (
+                  <li key={nav.id} className={`font-poppins font-normal cursor-pointer 
+                      text-[16px] ${index === navLinks.length - 1 ? 'mr-0 ' : 'mb-4'} text-white`}>
+                    <a href={`#${nav.id}`}>
+                      {nav.title}
+                    </a>
+                  </li>
+                ))}
+
+              </ul>
+
+            </div>
       </div>
     </nav>
   )
