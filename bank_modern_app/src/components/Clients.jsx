11714fd4e95572aa302712d7aa88a3efa8cb48c3
MbarekManouze <ecloesbac2020@gmail.com>
2023-12-29 19:31:15 +0100

    Project finished ---> still going to update

--- bank_modern_app/src/components/Clients.jsx
+++ bank_modern_app/src/components/Clients.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import { airbnb, binance, coinbase, dropbox } from '../assets'
+import styles from '../style'
+import { clients } from '../constants'
 
-const Clients = () => {
-  return (
-    <div>Clients</div>
-  )
-}
+const Clients = () => (
+
+  <section className={`${styles.flexCenter} my-4`}>
+    <div className={`${styles.flexCenter} flex-wrap w-full`}>
+      {clients.map((client) => (
+        <div key={client.id} className={`flex-1 ${styles.flexCenter} sm:min-w-[192px] min-w-[120px🚨 Important announcement : Geeksblabla Linkedin page was stolen 🚨🚨 Important announcement : Geeksblabla Linkedin page was stolen 🚨]`}>
+          <img src={client.logo} alt="client" className="sm:w-[192px] w-[100px] object-contain"/>
+        </div>
+      ))}
+    </div>
+  </section>
+
+)
 
 export default Clients
